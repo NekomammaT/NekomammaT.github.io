fa24b4868df9da0309f824608d9b59c0918adcdd
Yuichiro Tada <yuichiro.t.tada@gmail.com>
2025-06-19 01:00:39 +0900

    background

--- js/background.js
+++ js/background.js
@@ -10,6 +10,12 @@ imgfile[4] = 'header-background5.jpg';
 imgfile[5] = 'header-background6.jpg';
 imgfile[6] = 'header-background7.jpg';
 imgfile[7] = 'header-background8.jpg';
+imgfile[8] = 'header-background9.jpg';
+imgfile[9] = 'header-background10.jpg';
+imgfile[10] = 'header-background11.jpg';
+imgfile[11] = 'header-background12.jpg';
+imgfile[12] = 'header-background13.jpg';
+imgfile[13] = 'header-background14.jpg';
 // 画像の数を元に、ランダムな数値を算出
 var n = Math.floor(Math.random() * imgfile.length);
 var bgbox = $('header');
